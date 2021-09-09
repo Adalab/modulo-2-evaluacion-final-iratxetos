657c0ee92e938d8caaf77ab325118bd2d7854bdb
iratxetos <iratxetxu@gmail.com>
2021-09-10 00:29:31 +0200

    added first beta

--- src/js/main.js
+++ src/js/main.js
@@ -5,31 +5,54 @@ const searchButton = document.querySelector('.js-search');
 const inputSerie = document.querySelector('.js-input');
 const seriesContainer = document.querySelector('.js-series-container');
 
-let series = [];
+let seriesList = [];
 
-// function completeUrl() {
-//     let userSerie = inputSerie.value;
-//     let url = `https://api.tvmaze.com/search/shows?q=${userSerie}`;
-//     return url;
+function completeUrl() {
+    let userSerie = inputSerie.value.toLowerCase();
+    let url = `//api.tvmaze.com/search/shows?q=${userSerie}`;
+    return url;
+}
+
+// function isValidSerie(serie) {
+//     // transformo el texto que ha introducido el usuario a minuscula
+//     const filterValue = filterInput.value.toLowerCase();
+//     // includes me devuelve un booleano
+//     // así que devuelvo lo que devuelve includes,
+//     // true si  contiene lo que ha escrito el usuario en el input false sino está incluido
+//     return serie.name.toLowerCase().includes(filterValue);
+//     console.log(isValidSerie(serie));
 // }
-// console.log(completeUrl);
 
-function getASerie() {
-    // let url = completeUrl();
-    let url = 'https://api.tvmaze.com/search/shows?q=girls';
+function getASerie(ev) {
+    let url = completeUrl();
+    //let url = 'https://api.tvmaze.com/search/shows?q=girls';
+
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            series = data;
-            console.log(series);
-            for (const serie of series) {
-                let seriesName = `<li>${serie.show.name}</li>`;
-                series += seriesName;
-                console.log(seriesName);
+            let series = data;
+
+            for (let serie of series) {
+
+                let foto = serie.show.image;
+                let seriesContent = '';
+                if (foto === null) {
+
+                    seriesContent = `<li><img src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV"/> ${serie.show.name}</li>`;
+                    seriesList += seriesContent;
+
+                } else {
+                    seriesContent = `<li><img src="${serie.show.image.original}"/> ${serie.show.name}</li>`;
+                    seriesList += seriesContent;
+
+                }
+
             }
-
-
         });
+
+    ev.preventDefault();
+    seriesContainer.innerHTML = seriesList;
+
 }
 
 searchButton.addEventListener('click', getASerie);
