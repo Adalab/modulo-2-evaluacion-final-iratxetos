fd87367f6827798bde42f49c1de93980f235d79c
iratxetos <iratxetxu@gmail.com>
2021-09-10 00:51:31 +0200

    created new branch

--- src/js/main.js
+++ src/js/main.js
@@ -13,16 +13,6 @@ function completeUrl() {
     return url;
 }
 
-// function isValidSerie(serie) {
-//     // transformo el texto que ha introducido el usuario a minuscula
-//     const filterValue = filterInput.value.toLowerCase();
-//     // includes me devuelve un booleano
-//     // así que devuelvo lo que devuelve includes,
-//     // true si  contiene lo que ha escrito el usuario en el input false sino está incluido
-//     return serie.name.toLowerCase().includes(filterValue);
-//     console.log(isValidSerie(serie));
-// }
-
 function getASerie(ev) {
     let url = completeUrl();
     //let url = 'https://api.tvmaze.com/search/shows?q=girls';
@@ -34,9 +24,9 @@ function getASerie(ev) {
 
             for (let serie of series) {
 
-                let foto = serie.show.image;
+                let photo = serie.show.image;
                 let seriesContent = '';
-                if (foto === null) {
+                if (photo === null) {
 
                     seriesContent = `<li><img src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV"/> ${serie.show.name}</li>`;
                     seriesList += seriesContent;
